38315ac61ffa3c404b9054a0762a2843e0d4b35c
maeda1150 <masaki19770108@gmail.com>
2018-04-30 18:15:03 +0900

    puts log & reteurn token

--- controllers/get_sample.go
+++ controllers/get_sample.go
@@ -1,11 +1,15 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func GetSample(c *gin.Context) {
 	params := c.Query("hoge")
+	token := c.Query("token")
 	c.JSON(200, gin.H{
 		"message": "get",
 		"params":  params,
+		"token":   token,
 	})
 }
--- controllers/post_sample.go
+++ controllers/post_sample.go
@@ -1,16 +1,27 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 type JSONParams struct {
-	Hoge string `form:"hoge" binding:"required"`
+	Hoge  string `form:"hoge" binding:"required"`
+	Token string `form:"token" binding:"required"`
 }
 
 func PostSample(c *gin.Context) {
+	log.Println("----------------------")
+	log.Println(c.Request.Header)
+	log.Println("----------------------")
+	log.Println(c.Request.Header["Content-Type"])
+	log.Println("----------------------")
 	var json JSONParams
 	c.Bind(&json)
 	c.JSON(200, gin.H{
 		"message": "post",
 		"params":  json.Hoge,
+		"token":   json.Token,
 	})
 }
